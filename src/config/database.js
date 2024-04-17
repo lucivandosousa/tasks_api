1a1cfb6314bc990ebef0e1acb17fa1363c23df3a
Lucivando Sousa <lucivandosousa@gmail.com>
2024-04-17 11:28:44 -0300

    fix: ajuste da conexao do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,11 @@
 const sqlite3 = require('sqlite3');
 
+const path = require("path")
+const pathDB = path.resolve("src", "database", "db.sqlite")
+
 const SQLite3 = sqlite3.verbose();
 
-const db = new SQLite3.Database("../database/db.sqlite", SQLite3.OPEN_READWRITE, (error) => {
+const db = new SQLite3.Database(pathDB, SQLite3.OPEN_READWRITE, (error) => {
   if (error) {
     console.log("error connecting to the database: " + error)
   }
